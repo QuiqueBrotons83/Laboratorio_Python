d3fc7af7af3af9654a268586551b69924685982c
nombre <quiquebrotons83@gmail.com>
2025-01-09 18:44:09 +0100

    subida mejoras

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Laboratorio_Python
+## Análisis Ingresos en Brasil
+Ejercicio de análisis sobre un data base de ingresos de Brasil donde realizo un estudio sobre los tipos de ingresos  y de donde provienen.
+## 📊 Análisis de Ingresos: Exploración y Modelado de Datos
+### 📖 Descripción
+En este proyecto se ha realizado un análisis detallado de los ingresos presupuestarios de Brasil entre 2013 y 2021. El objetivo es la identificación de posibles insights en las diferentes csv por años y sacar conclusiones de como o que ha podido suceder en esos 7 años.
+
+
+## 🗂️ Estructura del Proyecto
+    
+    ├── 01-datos/                     # Carpeta donde contiene los datos que voy a trabajar
+    ├── 01-notebook/                  # Traabajos realizados
+    ├── 02-src/                       # SRC para la creación y utlización de definiciones que me ayudan a realizar consultas más optimas.
+    ├── README.md                     # Descripción del proyecto
+## 📊 Resultados y Conclusiones
+La conclusiones de esta base de datos las podemos ver en la carpeta notebook donde podremos analizar y sacar las mejores conclusiones de ella.
+
+## ✒️ Autor
+Quique Brotons - @Quique Brotons
--- src/soporte_funciones.py
+++ src/soporte_funciones.py
@@ -1,7 +1,29 @@
-import pandas as pd
+# En este documento nos encontramos con las funciones creadas para trabajar en el archivo de análisis de presupuestos de Brasil
 
-def abrir_csv(ruta, separador=';', codificacion='latin1'):
-    return pd.read_csv(ruta,sep=separador,encoding=codificacion)
+import pandas as pd
+import numpy as np
+from pandas.api.types import is_datetime64_any_dtype as is_datetime
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def info_df(dataframe):
+    """
+    Función que devuelve información general sobre el DatFrame que le pasemos.
+
+    Args:
+        df (DataFrame): DataFrame con información que queramos revisar
+
+    Returns:
+        DataFrame: DataFrame con información general sobre las columnas del DataFrame que se le ha pasado a la función: tipo de datos, número de
+        registros, número de valores nulos, porcentaje de los valores nulos sobre el total
+    """
+    info_df = pd.DataFrame()
+    info_df["Tipo_dato"] = dataframe.dtypes
+    info_df["numero_registros"] = [dataframe[elemento].value_counts().sum() for elemento in dataframe]
+    info_df["Numero_nulos"] = round(dataframe.isnull().sum())
+    info_df["%_nulos"] = round((dataframe.isnull().sum()/dataframe.shape[0])*100, 2)
+
+    return info_df
 
     
 
